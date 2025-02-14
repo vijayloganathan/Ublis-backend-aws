1ea1b34f71df2a32ce392ed915957884987b761f
vijayloganathan <vijayloganathan2002@gmail.com>
2025-02-14 23:28:42 +0530

    6/2/25 VJ AN

--- src/server.ts
+++ src/server.ts
@@ -10,43 +10,43 @@ const init = async () => {
   try {
     DotEnv.config();
 
-    const server = Hapi.server({
-      host: "0.0.0.0",
-      port: 6201,
-      routes: {
-        cors: {
-          origin: [
-            "65.1.147.86",
-            "13.203.86.57",
-            "65.2.63.226",
-            "13.203.158.161",
-          ], // Allowed origins
-          headers: ["Accept", "Authorization", "Content-Type", "If-None-Match"], // Allowed headers
-          exposedHeaders: ["WWW-Authenticate", "Server-Authorization"], // Exposed headers
-          credentials: true, // Allow credentials (cookies/auth headers)
-        },
-        payload: {
-          maxBytes: 5242880,
-        },
-      },
-    });
-
     // const server = Hapi.server({
-    //   host: process.env.HOST || "localhost",
-    //   port: process.env.PORT || 6201,
+    //   host: "0.0.0.0",
+    //   port: 6201,
     //   routes: {
     //     cors: {
-    //       origin: ["*"],
-    //       headers: ["Accept", "Authorization", "Content-Type", "If-None-Match"],
-    //       exposedHeaders: ["WWW-Authenticate", "Server-Authorization"],
-    //       credentials: true,
+    //       origin: [
+    //         "65.1.147.86",
+    //         "13.203.86.57",
+    //         "65.2.63.226",
+    //         "13.203.158.161",
+    //       ], // Allowed origins
+    //       headers: ["Accept", "Authorization", "Content-Type", "If-None-Match"], // Allowed headers
+    //       exposedHeaders: ["WWW-Authenticate", "Server-Authorization"], // Exposed headers
+    //       credentials: true, // Allow credentials (cookies/auth headers)
     //     },
-    //     security: true,
     //     payload: {
     //       maxBytes: 5242880,
     //     },
     //   },
     // });
+
+    const server = Hapi.server({
+      host: process.env.HOST || "localhost",
+      port: process.env.PORT || 6201,
+      routes: {
+        cors: {
+          origin: ["*"],
+          headers: ["Accept", "Authorization", "Content-Type", "If-None-Match"],
+          exposedHeaders: ["WWW-Authenticate", "Server-Authorization"],
+          credentials: true,
+        },
+        security: true,
+        payload: {
+          maxBytes: 5242880,
+        },
+      },
+    });
 
     // REGISTER HAPI ROUTES
     await Router.loadRoutes(server);
